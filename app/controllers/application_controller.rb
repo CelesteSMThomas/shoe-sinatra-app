fa97f7135e903cc996abe331023cd7664d92bf38
celestesmthomas <celestesmthomas@gmail.com>
2020-08-16 13:42:59 -0700

    Created nav in layout.erb and enhanced ux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,30 @@ class ApplicationController < Sinatra::Base
     #enable sessions hash
     enable :sessions
     # set session secret for an extra layer of security
-    set :session_secret, "supercalifragilisticexpialidocious"
+    set :session_secret, "super_cali_fragilistic_expialidocious"
   end
 
   get "/" do
+    #if you're logged in you see your home page
+    if logged_in? 
+      redirect "/users/#{current_user.id}"
+    end
+    #else you see the welcome page
     erb :welcome
+  end
+
+  helpers do
+    #find user by the key/value pair created in post '/login' session hash
+    def current_user
+      #keeps track of the logged in user
+        User.find_by(id: session[:user_id])
+    end
+
+#returns a boolean(true/false) if the user is logged in or not
+    def logged_in?
+      #it is a double bang to convert to a hard true or hard false
+        !!current_user
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
     #render login form
-    get "/login" do
+    get '/login' do
         erb :login
     end
 
     # recieve(post) the data (params) from the login form
-    post "/login" do
+    post '/login' do
         # how to authenticate the user's email and password
         # find the user(find_by activerecord method) by their email
         user = User.find_by(email: params[:email])
@@ -18,13 +18,43 @@ class UsersController < ApplicationController
             redirect "/users/#{user.id}"
         else
             #show an error message and redirect to the login screen to try again
-            redirect "/login"
+            redirect "/users/login"
         end
     end
 
         #users SHOW route
-        get "/users/:id" do #this is a dynamic route
-        "users show page!"
+        get '/users/:id' do #this is a dynamic route
+    # find the user
+        @user = User.find_by(id: params[:id])
+        erb:'/users/show'
+        end
+
+        # SIGN UP ROUTE
+        # get sign up route that renders the signup form
+        get '/signup' do
+            #render my sign form erb
+            erb :'/users/signup'
+        end
+
+        #post sign up route that recieves the input data from the user, creates the user, and logs the user in
+        post '/users' do
+            # will add validations to inputs later to make sure all inputs are filled out prior to creating user
+            @user = User.create(params)
+            #this allows us to use all params as our argument in the user.rb file when creating a new user.
+            #post sign up route to creat user using params and add key/value pair to sessions hash
+            session[:user_id] = @user.id
+            # redirect to user profile
+            redirect "/users/#{@user.id}"
+        end
+
+
+        #LOG OUT 
+        #get logout that clears the session hash
+        get '/logout' do
+            session.clear
+
+            #redirect back to homepage
+            redirect "/"
         end
         
 
